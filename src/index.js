9b6e13fec3cbf8e9d1c5cde3a5c48bff85a6cb9d
Tanner <troxs690@gmail.com>
2022-02-08 13:09:36 -0600

    Done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.querySelector('form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+const newTask = document.getElementById('new-task-description').value
+const textValue = document.getElementById('tasks')
+const newLi = document.createElement('li')
+newLi.innerText = newTask
+textValue.appendChild(newLi)
+
+
+
+
+
+
+  })})
